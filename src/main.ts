1b07dde464f4042ed3251cc333e246c57e8a4ebf
Rajat Singhal <rajat@RajatMac.guruestate.com>
2019-01-23 19:09:44 +0700

    swagger_init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Test API')
+    .setDescription('Playground for NEST Framework development')
+    .setVersion('1.0')
+    .build();
+  const swagger_document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/swagger', app, swagger_document);
   await app.listen(3000);
 }
 bootstrap();
